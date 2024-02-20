ac225a7e25b770e0fd96442746a8a76850092161
Davis Sampaio <davislamenha@gmail.com>
2024-02-20 10:09:24 -0300

    not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { RotaUmComponent } from './rota-um/rota-um.component';
 import { RotaDoisComponent } from './rota-dois/rota-dois.component';
 import { RotaTresComponent } from './rota-tres/rota-tres.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: 'rota-um', component: RotaUmComponent },
+  { path: '', component: RotaUmComponent },
   { path: 'rota-dois', component: RotaDoisComponent },
   { path: 'rota-tres', component: RotaTresComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
